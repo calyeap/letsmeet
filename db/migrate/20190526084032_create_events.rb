fc17028e5a66e218aa284957ce6ab227dc2e718c
calyeap <calvinyeap.h@gmail.com>
2019-05-27 19:28:27 +0800

    lots of stuff

--- db/migrate/20190526084032_create_events.rb
+++ db/migrate/20190526084032_create_events.rb
@@ -3,7 +3,6 @@ class CreateEvents < ActiveRecord::Migration[5.2]
     create_table :events do |t|
       t.string :name
       t.text :description
-      t.references :user, polymorphic: true
       t.boolean :choices
       t.datetime :date
 
